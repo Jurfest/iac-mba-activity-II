006d54db48c4bf97db9fd183ae3fd73f06452ecc
Jurfest <jurfest.apps@gmail.com>
2024-04-27 03:19:23 -0300

    feat: add ingress rule

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,15 @@ resource "aws_security_group" "instance_sg" {
   }
 
   ingress {
-    from_port   = 80 # 27017 - MongoDB default port
-    to_port     = 80 # 27017 - MongoDB default port
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 27017 # MongoDB default port
+    to_port     = 27017 # MongoDB default port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
